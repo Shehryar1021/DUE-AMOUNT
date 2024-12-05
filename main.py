3c782050fdef9f4fe3c9a87eac2e341c522d3ee1
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-12-05 16:56:56 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+customer = str(input("Customer's Name:"))
+print ("Welcome to our store")
+print ("This is our products")
+print ("Orange 0.5$")
+print ("Apple 1$")
+print ("Grape 0.5$")
+print ("Banana 0.5$")
+prices = {"orange":0.5,"apple":1,"grape":0.5,"banana":0.5}
+productnum = input("How many things do you want to buy: ") wants
+while not productnum.isdigit():
+    productnum = input("How many different products do you want to buy: ")
+productnum = int(productnum)
+totalprice = 0
+totalfruit = 0
+print('         BILL')
+for i in range(productnum):
+    chosenproduct = input("What do you want to buy: ").lower()
+    while not chosenproduct in ['orange','apple','banana','grape']:
+        chosenproduct = input("What do you want to buy: ").lower()
+    fruitnum = input("How many of that do you want to buy: ")
+    while not fruitnum.isdigit():
+        fruitnum = input("How many of that do you want to buy: ")
+    fruitnum = int(fruitnum)
+    totalfruit += fruitnum
+    price = fruitnum * prices[chosenproduct]
+    totalprice += price
+    startspaces = ' ' * (11 - len(chosenproduct))
+    endspaces = ' ' * (5 - len(str(fruitnum)))
+    print(chosenproduct.capitalize() + startspaces + str(fruitnum) + endspaces + str(price) + '$')
+
+print('Total:     ' + str(totalfruit) + ' ' * (5 - len(str(totalprice))) + str(totalprice) + '$')
